d859ccf64fb2ada30b94a8591beca59ebb6230e4
Jonathan Duran <headjd@gmail.com>
2018-06-13 11:28:53 -0400

    Interpolation

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -4,7 +4,13 @@ import { Component, OnInit } from '@angular/core';
   selector: '[app-test]',
   //templateUrl: './test.component.html',
   template: `
-    <div>Hola JDDS</div>
+    <h2>{{ name }}</h2>
+    <h3>{{ 2+2 }}</h3>
+    <h3>{{ "Msj:" + msj }}</h3>
+    <h3>{{ msj.length }}</h3>
+    <h3>{{ msj.toUpperCase() }}</h3>
+    <h3>{{ greetUser() }}</h3>
+    <h3>{{ siteUrl }}</h3>
   `,
   //styleUrls: ['./test.component.css'],
   styles: [
@@ -16,7 +22,15 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class TestComponent implements OnInit {
+  public name = 'I am Component Test';
+  public msj = 'Hola Worl';
+  public user = 'Jonathan Duran';
+  public siteUrl = window.location.href;
   constructor() {}
 
   ngOnInit() {}
+
+  greetUser() {
+    return 'Hello ' + this.user;
+  }
 }
